d093dd2f2674ad7e13dedd35fcd00a66b28655cc
Gorka <gorka@540deg.com>
2018-02-19 17:18:37 +0100

    Initial commit

---
+++ src/calculator.ts
@@ -0,0 +1,10 @@
+export default class Calculator {
+
+    public add(number1: number, number2: number): number {
+        return 0;
+    }
+
+    public multiply(number1: number, number2: number): number {
+        return 0;
+    }
+}
