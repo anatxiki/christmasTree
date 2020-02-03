5a61da9467c74f64d6645a6ca524ed8eb1930c8e
Gorka <gorka@540deg.com>
2020-02-03 22:38:56 +0100

    Fix ts warning

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,10 +1,10 @@
 export default class Calculator {
 
-    public add(number1: number, number2: number): number {
+    add(number1: number, number2: number): number {
         return 0;
     }
 
-    public multiply(number1: number, number2: number): number {
+    multiply(number1: number, number2: number): number {
         return 0;
     }
 }
