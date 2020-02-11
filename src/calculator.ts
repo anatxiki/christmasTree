91ecab51a9c031904af8e4864621d9e954c89424
Gorka <gorka@540deg.com>
2020-02-11 16:32:29 +0100

    Fix example

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,10 +1,9 @@
 export default class Calculator {
-
     add(number1: number, number2: number): number {
-        return 0;
+        return 0
     }
 
     multiply(number1: number, number2: number): number {
-        return 0;
+        return 0
     }
 }
--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -1,14 +1,13 @@
-import Calculator from '../src/calculator';
+import Calculator from '../src/calculator'
 
 describe('Calculator', () => {
-
-    const calculator = new Calculator();
+    const calculator = new Calculator()
 
     it('should add two numbers', () => {
-        expect(calculator.add(1, 2)).toEqual(3);
-    });
+        expect(calculator.add(1, 2)).toEqual(3)
+    })
 
     it('should multiply two numbers', () => {
-        expect(calculator.multiply(1, 2)).toEqual(2);
-    });
-});
+        expect(calculator.multiply(1, 2)).toEqual(2)
+    })
+})
